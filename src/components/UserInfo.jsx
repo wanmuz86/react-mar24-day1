4a479de78f91e2ad0dc20e27b0c2912de667b313
Wan Muzaffar <wanmuz86@gmail.com>
2024-04-02 09:42:34 +0800

    conditional rendering

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 
 // Object destructuring of the props
-const UserInfo = ({user}) => {
+const UserInfo = ({ user }) => {
     // Object descturing of user
-    const {id, name, age} = user;
-  return (
-    <div>
-        <p>User Id:  {id}</p>
-        <p>Name: {name } </p>
-        <p>Age: {age } </p>
-    </div>
-  )
+    const { id, name, age } = user;
+    return (
+        <div>
+            <p>User Id:  {id}</p>
+            <p>Name: {name} </p>
+            <p>Age: {age} </p>
+
+            {
+                age < 18 ?
+                    <p>Minor: Please obtain parental consent!</p>
+                    :
+                    <p>Adult: Welcome to our platform</p>
+            }
+        </div>
+    )
 }
 
 export default UserInfo
